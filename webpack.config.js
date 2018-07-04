2ffeac68a8702245ccd5796d8ffe3c9504ffa710
yu chiu <yuchiu2002@hotmail.com>
2018-07-03 23:32:34 -0400

    disabled eslint on webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,81 @@
-var webpack = require('webpack')
-var path = require('path')
-var htmlWebpackPlugin = require('html-webpack-plugin')
-var OpenBrowserPlugin = require('open-browser-webpack-plugin')
+/* eslint-disable */
+
+let webpack = require("webpack");
+let path = require("path");
+let htmlWebpackPlugin = require("html-webpack-plugin");
+let OpenBrowserPlugin = require("open-browser-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: path.join(__dirname, 'src', 'index.js')
+    app: path.join(__dirname, "src", "index.js")
   },
 
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    sourceMapFilename: '[name].bundle.map'
+    path: path.join(__dirname, "dist"),
+    filename: "[name].bundle.js",
+    sourceMapFilename: "[name].bundle.map"
   },
 
-  devtool: '#source-map',
+  devtool: "#source-map",
 
   module: {
     loaders: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-          presets: ['react', 'es2015']
+          presets: ["react", "es2015"]
         }
-      }, {
+      },
+      {
         test: /\.(css|scss|sass)$/,
-        loader: 'style-loader!css-loader!postcss-loader!sass-loader',
-        include: path.join(__dirname, 'assets', 'scss')
-      }, {
+        loader: "style-loader!css-loader!postcss-loader!sass-loader",
+        include: path.join(__dirname, "assets", "scss")
+      },
+      {
         test: /\.(png|jpg)$/,
-        loader: 'file-loader',
-        include: path.join(__dirname, 'assets', 'img')
+        loader: "file-loader",
+        include: path.join(__dirname, "assets", "img")
       }
     ]
   },
 
   devServer: {
     historyApiFallback: true,
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     inline: true,
     hot: true,
-    stats: 'errors-only'
+    stats: "errors-only"
   },
 
-  plugins: process.env.NODE_ENV === 'production'
-    ? [
-      new webpack.DefinePlugin({
-        'process.env': {
-          'NODE_ENV': JSON.stringify('production')
-        }
-      }),
-      new webpack
-        .optimize
-        .UglifyJsPlugin({
-          minimize: true,
-          compress: {
-            warnings: true,
-            drop_console: true
-          }
-        }),
-      new htmlWebpackPlugin({
-        template: path.join(__dirname, 'index.html'),
-        hash: true
-      })
-    ]
-    : [
-      new htmlWebpackPlugin({
-        template: path.join(__dirname, 'index.html'),
-        hash: true
-      }),
-      new webpack.HotModuleReplacementPlugin(),
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [
+          new webpack.DefinePlugin({
+            "process.env": {
+              NODE_ENV: JSON.stringify("production")
+            }
+          }),
+          new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+              warnings: true,
+              drop_console: true
+            }
+          }),
+          new htmlWebpackPlugin({
+            template: path.join(__dirname, "index.html"),
+            hash: true
+          })
+        ]
+      : [
+          new htmlWebpackPlugin({
+            template: path.join(__dirname, "index.html"),
+            hash: true
+          }),
+          new webpack.HotModuleReplacementPlugin(),
 
-      new OpenBrowserPlugin({url: 'http://localhost:8080'})
-    ]
-}
+          new OpenBrowserPlugin({ url: "http://localhost:8080" })
+        ]
+};
